9f68ada159e91b72f9da1344d5e8ba8517a1dba0
Shiny1412 <les.017ciers@gmail.com>
2026-01-09 15:14:12 +0700

    1st : Setup BE

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    
+    // 👇 Ubah ini agar spesifik ke URL Frontend
+    'allowed_origins' => ['http://localhost:3000'], 
+
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false, 
+];
