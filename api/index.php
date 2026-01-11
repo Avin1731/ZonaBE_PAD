01cb1bac2a2aa49f7e10ff02690900f32fbb4b3b
Shiny1412 <les.017ciers@gmail.com>
2026-01-11 09:36:15 +0700

    Fix Vercel: Force JSON request in entry point and stabilize config

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
-// Pastikan path ini benar menunjuk ke folder public
+// Trik Vercel: Paksa request jadi JSON agar Laravel tidak redirect ke 'login' route
+if (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] === '*/*') {
+    $_SERVER['HTTP_ACCEPT'] = 'application/json';
+}
+
 require __DIR__ . '/../public/index.php';
