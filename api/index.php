d7c8a5c8a52d966d143fb4ed1724fb98ce4759f8
Shiny1412 <les.017ciers@gmail.com>
2026-01-11 10:04:11 +0700

    SUNGGUH MALAS MENANGGAPI

--- api/index.php
+++ api/index.php
@@ -1,8 +1,2 @@
 <?php
-
-// Trik Vercel: Paksa request jadi JSON agar Laravel tidak redirect ke 'login' route
-if (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] === '*/*') {
-    $_SERVER['HTTP_ACCEPT'] = 'application/json';
-}
-
 require __DIR__ . '/../public/index.php';
